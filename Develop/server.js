b923451d3f2ab0aaa852ab15d847d5ec1e6e81f2
School <kaboy@icloud.com>
2021-12-02 21:20:19 -0600

    new note saves on the request body

--- Develop/server.js
+++ Develop/server.js
@@ -16,9 +16,18 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+app.get('/api/notes', (req, res) => res.json(notesData));
 app.get('*', (req, res) => { //everything else — ignores everything after /
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+app.post('/api/notes', (req, res) => {
+  console.log(notesData)
+  console.log(req.body)
+  notesData.push(req.body)
+  console.log(notesData)
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesData))
+  res.json(req.body)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
